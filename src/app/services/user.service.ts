d7be19933cd3b8574430507fd40bcbbc42766295
Poorva224 <poorvashinde224@gmail.com>
2021-09-05 18:16:14 +0530

    New Project

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+
+  public addUser(user: any) {
+    return this.http.post(`${baseUrl}/user/`, user);
+  }
+
+  public deleteUser(id){
+    return this.http.delete(`${baseUrl}/quiz/${id}`);
+  }
+
+}
